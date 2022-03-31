d6f2a9678a92c094c70535455fcf69794a136c3c
SudalaiMani09 <msudalaimani92@gmail.com>
2022-03-31 13:00:31 +0530

    Second Push

---
+++ src/org/clean/Asia.java
@@ -0,0 +1,19 @@
+package org.clean;
+
+public class Asia {
+
+	private void india() {
+		System.out.println("2nd largest populated");
+	}
+	
+	private void china() {
+		System.out.println("largest populated");
+
+	}
+	
+	public static void main(String[] args) {
+		Asia a = new Asia();
+		a.india();
+		a.china();
+	}
+}
